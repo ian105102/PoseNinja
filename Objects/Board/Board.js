c8cdeff524756bc47240bb33b638d337aaf78afd
ian1105102 <ian105102@gmail.com>
2025-05-25 13:59:36 +0800

    更新 Board 類別的構造函數以接受 x 和 y 參數，並在 EasyBoard 中調整板子生成邏輯以使用這些參數

--- Objects/Board/Board.js
+++ Objects/Board/Board.js
@@ -2,7 +2,7 @@ export class Board{
     constructor(x = 0, y = 0) {
         this.type =0;
         this.color = "rgba(255, 245, 245, 0.57)";
-        this.x = 0;
-        this.y = 0;
+        this.x = x;
+        this.y = y;
     }
 }
